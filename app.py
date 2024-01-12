687ac45f179d573d944d70fdd1197b5284f662a0
Victor Feliciano <115307935+VetVictor33@users.noreply.github.com>
2024-01-12 09:13:33 -0300

    add delete

--- app.py
+++ app.py
@@ -8,22 +8,33 @@ books = [
   {'id':'3', 'title':'I, Robot', 'author': 'Isaac Asimov'}
 ]
 
+#CREATE
 @app.route('/books', methods=['POST'])
 def create_book():
   new_book = request.get_json()
   books.append(new_book)
   return jsonify(books)
 
+#READ
 @app.route('/books', methods=['GET'])
 def read_books():
   return jsonify(books)
 
+#UPDATE
 @app.route('/books/<int:id>', methods=['PUT'])
 def update_books(id):
   altered_book = request.get_json()
   for index, book in enumerate(books):
     if str(book.get('id')) == str(id):
       books[index].update(altered_book)
+  return jsonify(books)
+
+#DELETE
+@app.route('/books/<int:id>', methods=['DELETE'])
+def delete_book(id):
+  for index, book in enumerate(books):
+    if str(book.get('id')) == str(id):
+      del books[index]
   return jsonify(books)
 
 app.run(port=5000, host='localhost', debug=True)
